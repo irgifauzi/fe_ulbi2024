4aecbbe9e1720d8a2e10c6861f73b828dc4a74ed
Irgi Fauzi <114649826+irgifauzi@users.noreply.github.com>
2024-04-26 16:16:27 +0700

    j

--- js/fetch.js
+++ js/fetch.js
@@ -1,4 +1,6 @@
 import { get } from "https://bukulapak.github.io/api/process.js";
+import { isiTablePresensi } from "./controller/get.js";
+import { urlAPI } from "./config/url.js";
 import { addInner } from "https://bukulapak.github.io/element/process.js";
 import { getRandomColor, getRandomColorName } from "https://bukulapak.github.io/image/process.js";
 import { isiTabel } from "./table.js";
